f9bc61ea0ddc790e5ebc925f061663eb968bce2f
PrinceG-2k6 <princegupta14311@gmail.com>
2025-03-06 10:33:03 +0530

    Add files via upload

---
+++ LAB work/lab 6/Q2-oddevenLinkedList.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node* next;
+};
+
+struct node* createNode(struct node* head){
+    struct node* Newnode = (struct node*)malloc(sizeof(struct node));
+    struct node* temp;
+    int data;
+    printf("Enter the data : ");
+    scanf("%d",&data);
+    Newnode->data = data;
+    Newnode->next = NULL;
+    if(head == NULL)
+    {
+        head = Newnode;
+    }
+    else
+    {
+        temp= head;
+        while (temp->next!=NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = Newnode;
+        
+    }
+    return head;
+}
+
+void printList(struct node* head)
+{
+    struct node* temp = head;
+    while(temp->next != NULL)
+    {
+        printf(" %d |",temp->data);
+        temp = temp->next;
+    }
+    printf(" %d ",temp->data);
+}
+void divideOddEven(struct node* head, struct node** oddhead, struct node** evenhead) {
+    struct node* oddTail = NULL;
+    struct node* evenTail = NULL;
+    struct node* current = head;
+
+    while (current != NULL) {
+        if (current->data % 2 == 0) {
+            if (*evenhead == NULL) {
+                *evenhead = current;
+                evenTail = current;
+            } else {
+                evenTail->next = current;
+                evenTail = evenTail->next;
+            }
+        } else {
+            if (*oddhead == NULL) {
+                *oddhead = current;
+                oddTail = current;
+            } else {
+                oddTail->next = current;
+                oddTail = oddTail->next;
+            }
+        }
+        current = current->next;
+    }
+
+    if (oddTail != NULL) {
+        oddTail->next = NULL;
+    }
+    if (evenTail != NULL) {
+        evenTail->next = NULL;
+    }
+}
+int main()
+{   
+    int i , n;
+    printf("Enter the number of nodes : ");
+    scanf("%d",&n);
+    struct node* head = NULL;
+    struct node* oddhead = NULL;
+    struct node* evenhead = NULL;
+    for(i=0;i<n;i++)
+    {
+        head = createNode(head);
+    }
+    printList(head);
+    divideOddEven(head, &oddhead, &evenhead);
+    printf("\nOdd List: ");
+    printList(oddhead);
+    printf("\nEven List: ");
+    printList(evenhead);
+    return 0 ;
+}
---
+++ LAB work/lab 6/Q6-ReverseLL.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node* next;
+};
+
+struct node* createNode(struct node* head){
+    struct node* Newnode = (struct node*)malloc(sizeof(struct node));
+    struct node* temp;
+    int data;
+    printf("Enter the data : ");
+    scanf("%d",&data);
+    Newnode->data = data;
+    Newnode->next = NULL;
+    if(head == NULL)
+    {
+        head = Newnode;
+    }
+    else
+    {
+        temp= head;
+        while (temp->next!=NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = Newnode;
+        
+    }
+    return head;
+}
+
+void printList(struct node* head)
+{
+    struct node* temp = head;
+    while(temp->next != NULL)
+    {
+        printf(" %d |",temp->data);
+        temp = temp->next;
+    }
+    printf(" %d \n",temp->data);
+}
+struct node* reverseList(struct node* head) {
+    struct node* prev = NULL;
+    struct node* current = head;
+    struct node* next = NULL;
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    head = prev;
+    return head;
+}
+int main()
+{   
+    int i , n;
+    printf("Enter the number of nodes : ");
+    scanf("%d",&n);
+    struct node* head = NULL;
+    for(i=0;i<n;i++)
+    {
+        head = createNode(head);
+    }
+    printList(head);
+    head = reverseList(head);
+    printList(head);
+    return 0 ;
+}
