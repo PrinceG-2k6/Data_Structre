fd9d68dbef85403f8376e2396c35d71b48fce171
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-24 17:04:53 +0530

    .

--- CodeChef/Arrays/Q18_MinToMax.cpp
+++ CodeChef/Arrays/Q18_MinToMax.cpp
@@ -1,31 +1,38 @@
-
 #include <bits/stdc++.h>
-
 using namespace std;
 
 class Solution {
 public:
     int countNonMinimum(vector<int>& nums) {
-        // write your code here 
-        
+        int min =1000;
+        int count =0;
+        for(int num:nums){
+            if(num<min){
+                min =num;
+            }
+        }
+        for(int num:nums){
+            if(num == min){
+                count++;
+            }
+        }
+        return nums.size()-count;
     }
 };
 
-
-int main()
-{
-
+int main() {
+    int t;
+    cin>>t;
     Solution sol;
-
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
+    while(t--){
+        int n,min;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        cout<< sol.countNonMinimum(a)<<endl;
     }
 
-    sol.check(arr);
-
-    return 0;
 }
+ 
