0d6c64035f23b8647db0b6ad980705ad12859386
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-27 18:54:20 +0530

    .\

--- CodeChef/Arrays/Q28_Snapchat.cpp
+++ CodeChef/Arrays/Q28_Snapchat.cpp
@@ -1,20 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        int a[n],b[n];
-        for(int i=0;i<n;i++){
-            cin>>a[i];
+class Solution
+{
+public:
+    int snapStreak(int *a, int *b,int n)
+    {
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] != 0 && b[i] != 0)
+            {
+                count++;
+            }
+            else
+            {
+                if (count > max)
+                {
+                    max = count;
+                    count = 0;
+                }
+            }
         }
-        for(int i=0;i<n;i++){
-            cin>>b[i];
-        }
-    }
-	// your code goes here
+        if (count > max)
+            max = count;
 
+        return max;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    Solution sol;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n], b[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cin >> b[i];
+        }
+        cout << sol.snapStreak(a, b,n) << endl;
+    }
 }
