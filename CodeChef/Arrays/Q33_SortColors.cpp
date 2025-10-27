c93f3341485d49c11c90ec03c46d1ad994d693af
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-27 19:54:09 +0530

    .

---
+++ CodeChef/Arrays/Q33_SortColors.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void sortColors(vector<int>& nums) {
+    int low = 0, mid = 0, high = nums.size() - 1;
+
+    while (mid <= high) {
+        if (nums[mid] == 0) {
+            swap(nums[low], nums[mid]);
+            low++;
+            mid++;
+        }
+        else if (nums[mid] == 1) {
+            mid++;
+        }
+        else { // nums[mid] == 2
+            swap(nums[mid], nums[high]);
+            high--;
+        }
+    }
+}
+
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sortColors(a);
+}
