d274e8126a41aade191b838299ba6803230400de
PrinceG-2k6 <princegupta14311@gmail.com>
2025-10-30 16:30:23 +0530

    .

---
+++ LeetCode/Q164_Maxgap.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        long long n = nums.size(); //To prevent integer overflow during index calculation
+        int maxi = *max_element(nums.begin(),nums.end());
+        int mini = *min_element(nums.begin(),nums.end());
+
+        if(maxi == mini){
+            return 0;
+        }
+
+        vector<int> bucketLargest(n,INT_MIN);
+        vector<int> bucketSmallest(n,INT_MAX);
+
+        for(int i=0;i<n;i++){
+            int index = (((nums[i] - mini)*(n-1))/(maxi - mini));
+            bucketLargest[index] = max(bucketLargest[index],nums[i]);
+            bucketSmallest[index] = min(bucketSmallest[index],nums[i]);
+        }
+
+        int lG = 0;
+        int i = 0, j = 1;
+        while(j<n){
+            if(bucketLargest[j] == INT_MIN){
+                j++;
+            }
+            else if(bucketLargest[i] == INT_MIN){
+                i++;
+            }
+            else{
+                lG = max(lG,bucketSmallest[j] - bucketLargest[i]);
+                i++,j++;
+            }
+        }
+
+        return lG;
+    }
+};
+
+int main(){
+
+    Solution sol;
+    vector<int> nums = {3,6,9,1};
+    cout<<sol.maximumGap(nums);
+    return 0;
+}
