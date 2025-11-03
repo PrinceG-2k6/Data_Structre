8573d0fe3db17c622c10f3e3543c5699db49f9ac
PrinceG-2k6 <princegupta14311@gmail.com>
2025-11-03 20:19:08 +0530

    .

---
+++ LeetCode/Q1578_Colorful.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) {
+        if(colors.size() == 1){
+            return 0;
+        }
+        int i = 0;
+        int j = i+1;
+        int ans = 0;
+        while(j<colors.size()){
+            if(colors[i] == colors[j]){
+                if(neededTime[i]<=neededTime[j]){
+                    ans+=neededTime[i];
+                    i=j;
+                    j++;
+                }
+                else{
+                    ans+=neededTime[j];
+                    j++;
+                }
+            }
+            else{
+                i=j;
+                j++;
+            }
+        }
+        return ans;
+    }
+};
+int main(){
+    Solution sol;
+    string colors = "aaaac";
+    vector<int> neededTime ={1,2,3,4,5};
+    cout<<sol.minCost(colors,neededTime);
+
+    return 0;
+}
