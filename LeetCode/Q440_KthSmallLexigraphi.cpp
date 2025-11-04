3de219dd416817bd95a2e14b4e7450a85d5159ef
PrinceG-2k6 <princegupta14311@gmail.com>
2025-11-04 19:58:15 +0530

    .

---
+++ LeetCode/Q440_KthSmallLexigraphi.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findKthNumber(int n, int k) {
+        if(k==1){
+            return 1;
+        }
+    }
+};
+int main()
+{
+    Solution sol;
+    int x = 13;
+    int k =2;
+    cout<<sol.findKthNumber(x,k);
+}
