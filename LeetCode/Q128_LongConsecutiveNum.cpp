a957a0023edd6ffadc64f818ba66807b0c4b7f41
PrinceG-2k6 <princegupta14311@gmail.com>
2025-11-05 17:51:49 +0530

    .

---
+++ LeetCode/Q128_LongConsecutiveNum.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if (nums.empty()) return 0;
+
+        unordered_set<int> s(nums.begin(), nums.end()); // store all numbers in a hash set
+        int longest = 0;
+
+        for (int num : s) {
+            // Start counting only if 'num' is the beginning of a sequence
+            if (!s.count(num - 1)) {
+                int current = num;
+                int count = 1;
+
+                // Count consecutive numbers
+                while (s.count(current + 1)) {
+                    current++;
+                    count++;
+                }
+
+                longest = max(longest, count);
+            }
+        }
+
+        return longest;
+    }
+};
+
+
+int main()
+{
+    Solution sol;
+    vector<int> nums = {1, 0, 1, 2};
+    /*
+        3,2,9,1,8,7
+        9,2,3,1,8,7
+
+        -
+        -
+
+        1,2,3,9,8,7
+    */
+    cout << sol.longestConsecutive(nums);
+}
