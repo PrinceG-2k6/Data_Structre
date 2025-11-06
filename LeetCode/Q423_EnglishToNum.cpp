5635011e64fc54859f9984a4ebab8f62662fc09c
PrinceG-2k6 <princegupta14311@gmail.com>
2025-11-06 15:13:50 +0530

    .

---
+++ LeetCode/Q423_EnglishToNum.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string originalDigits(string s) {
+        string ans="";
+        
+    }
+};
+
+int main(){
+    Solution sol;
+    cout<<sol.originalDigits("owoztneoer");
+    
+}
