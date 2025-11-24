b92f286f8a102aadab607c4919e04f0ebbca0403
PrinceG-2k6 <princegupta14311@gmail.com>
2025-11-25 00:11:46 +0530

    .

---
+++ Try/polynomial.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int coef;
+    int exp;
+    struct node *next;
+} poly;
+
+poly * create(poly* head,int c,int e){
+    poly * n = (poly*)malloc(sizeof(poly));
+    n->coef=c;
+    n->exp=e;
+    if(head==NULL || e>head->exp){
+        n->next=head;
+        head = n;
+    }else{
+        poly * temp =head;
+        while(temp->next!=NULL && temp->next->exp>=e){
+            temp=temp->next;
+        }
+        n->next = temp->next;
+        temp->next=n;
+    }
+    return head;
+}
+
+void print(poly* head){
+    poly * temp =head;
+    while(temp->next!=NULL){
+        printf("%dx^%d + ",temp->coef,temp->exp);
+        temp=temp->next;
+    }
+    printf("%dx^%d",temp->coef,temp->exp);
+}
+
+int main(){
+    poly* head = NULL;
+    head = create(head,5,2);
+    
+    head = create(head,3,7);
+    
+    head = create(head,2,3);
+    
+    head = create(head,1,1);
+
+    print(head);
+
+    return 0;
+}
--- Try/tempCodeRunnerFile.c
+++ Try/tempCodeRunnerFile.c
@@ -1,60 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-void PrintArray(int r,int c,int a[r][c]){
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-            printf("%d\t",a[i][j]);
+
+typedef struct node
+{
+    int coef;
+    int exp;
+    struct node *next;
+} poly;
+
+poly * create(poly* head,int c,int e){
+    poly * n = (poly*)malloc(sizeof(poly));
+    n->coef=c;
+    n->exp=e;
+    if(head==NULL){
+        head = n;
+    }else{
+        poly * temp =head;
+        while(temp->next!=NULL && temp->next->coef>=c){
+            temp=temp->next;
         }
-        printf("\n");
+        temp->next=n;
     }
-};
+    return head;
+}
+
+void print(poly* head){
+    poly * temp =head;
+    while(temp->next!=NULL){
+        printf("%dx^%d + ",temp->coef,temp->exp);
+        temp=temp->next;
+    }
+    printf("%dx^%d",temp->coef,temp->exp);
+}
+
 int main(){
-    int a[3][3];
-    int b[3][3];
-    int nz1=0,nz2=0;
-
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            scanf("%d",&a[i][j]);
-            if(a[i][j]==0){
-                nz1++;
-            }
-        }
-    }
+    poly* head = NULL;
+    head = create(head,5,2);
     
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            scanf("%d",&b[i][j]);
-            if(b[i][j]==0){
-                nz2++;
-            }
-        }
-    }
-    PrintArray(3,3,a);
-    int s1[nz1][3];
-    int count;
-    count =0;
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            if(count!=0){
-                s1[count][0]=i;
-                s1[count][1]=j;
-                s1[count][2]=a[i][j];
-                count++;
-            }   
-        }
-    }
+    head = create(head,3,7);
+    
+    head = create(head,2,3);
+    
+    head = create(head,1,1);
 
-    int s2[nz2][3];
-    count =0;
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            if(count!=0){
-                s2[count][0]=i;
-                s2[count][1]=j;
-                s2[count][2]=b[i][j];
-                count++;
-            }   
-        }
-    }
+    print(head);
+
+    return 0;
 }
