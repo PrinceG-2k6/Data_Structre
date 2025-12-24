5c4dedd805d2b1f1c6b8aa0dad0a93fc5a3180c2
PrinceG-2k6 <princegupta14311@gmail.com>
2025-12-24 19:23:31 +0530

    .

--- LeetCode/Q10_RegularExpression.cpp
+++ LeetCode/Q10_RegularExpression.cpp
@@ -4,12 +4,44 @@ using namespace std;
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        unordered_map<char,int> mpp;
-        for(int i =0;i<s.length();i++){
-            mpp[s[i]]++;
+        int n = s.length();
+        int m = p.length();
+
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+        dp[0][0] = true;
+
+        // Handle patterns like a*, a*b*, a*b*c*
+        for (int j = 2; j <= m; j++) {
+            if (p[j - 1] == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
         }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                // Case 1: exact match or '.'
+                if (p[j - 1] == s[i - 1] || p[j - 1] == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+
+                // Case 2: '*'
+                else if (p[j - 1] == '*') {
+                    // zero occurrence of previous char
+                    dp[i][j] = dp[i][j - 2];
+
+                    // one or more occurrence
+                    if (p[j - 2] == s[i - 1] || p[j - 2] == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
+
 
 
 int main(){
