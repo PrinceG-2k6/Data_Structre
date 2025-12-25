10cb2dc638b3b75127e4e70fba8b9120d28c74d5
PrinceG-2k6 <princegupta14311@gmail.com>
2025-12-25 20:53:54 +0530

    .

--- LeetCode/Q16_3SumClosest.cpp
+++ LeetCode/Q16_3SumClosest.cpp
@@ -8,7 +8,7 @@ public:
         sort(nums.begin(), nums.end());
 
         int closest = nums[0] + nums[1] + nums[2];
-
+        
         for (int i = 0; i < n - 2; i++) {
             int l = i + 1, r = n - 1;
             while (l < r) {
