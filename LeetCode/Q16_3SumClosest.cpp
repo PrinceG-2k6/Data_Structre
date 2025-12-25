ca83b8264543d57f40e8b7290fee60c0789760db
PrinceG-2k6 <princegupta14311@gmail.com>
2025-12-25 16:47:36 +0530

    .

---
+++ LeetCode/Q16_3SumClosest.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {-1,2,1,-4};
+    cout<<sol.threeSumClosest(nums,1)<<endl;
+}
