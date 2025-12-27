a50203d09a598033e036c6b79bf6a4a10ab5cdd3
PrinceG-2k6 <princegupta14311@gmail.com>
2025-12-27 12:09:11 +0530

    .

--- LeetCode/Q2402_MeetingRooms.cpp
+++ LeetCode/Q2402_MeetingRooms.cpp
@@ -3,44 +3,71 @@ using namespace std;
 
 class Solution {
 public:
+    struct Request {
+        int start, end, id;
+    };
+
     int mostBooked(int n, vector<vector<int>>& meetings) {
-        sort(meetings.begin(), meetings.end());
-        vector<int> count(n, 0);
-        priority_queue<int, vector<int>, greater<int>> freeRoom;
-        for (int i = 0; i < n; i++){
-            freeRoom.push(i);
+        int m = meetings.size();
+        vector<int> result(m);
+        vector<int> cnt(n, 0);
+        vector<Request> reqs;
+
+        // Create request list
+        for (int i = 0; i < m; ++i) {
+            reqs.push_back({meetings[i][0], meetings[i][1], i});
         }
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> used;
-        for (auto& m : meetings){
-            long long start = m[0], end = m[1];
-            while (!used.empty() && used.top().first <= start){
-                int room = used.top().second;
-                used.pop();
-                freeRoom.push(room);
+
+        // Sort by start time
+        sort(reqs.begin(), reqs.end(), [](const Request& a, const Request& b) {
+            return a.start < b.start;
+        });
+
+        // Min-heap of available room IDs
+        priority_queue<int, vector<int>, greater<>> avail;
+
+        // Min-heap of (endTime, roomID)
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> assign;
+
+        for (int i = 0; i < n; ++i) avail.push(i);
+
+        for (auto& request : reqs) {
+            long long start = request.start, end = request.end;
+
+            // Free up rooms that have completed their meetings
+            while (!assign.empty() && start >= assign.top().first) {
+                avail.push(assign.top().second);
+                assign.pop();
             }
-            long long dur = end - start;
-            int room;
-            long long begin;
-            if (!freeRoom.empty()){
-                room = freeRoom.top();
-                freeRoom.pop();
-                begin = start;
+
+            if (!avail.empty()) {
+                // Assign a free room
+                int roomid = avail.top();
+                avail.pop();
+                cnt[roomid]++;
+                result[request.id] = roomid;
+                assign.push({end, roomid});
             } else {
-                auto [delay, room2] = used.top();
-                used.pop();
-                room = room2;
-                begin = delay;
-            }
-            count[room]++;
-            used.push({begin + dur, room});
-        }
-        int ans = 0;
-        for (int i = 0; i < n; i++){
-            if (count[i] > count[ans]){
-                ans = i;
+                // Extend the meeting in the earliest available room
+                auto [freeTime, room] = assign.top();
+                assign.pop();
+                assign.push({freeTime + (end - start), room});
+                cnt[room]++;
+                result[request.id] = room;
             }
         }
-        return ans;
+
+        // Find room with maximum meetings
+        int maxi = -1;
+        int index = -1;
+        for (int i = 0; i < n; ++i) {
+            if (cnt[i] > maxi) {
+                index = i;
+                maxi = cnt[i];
+            }
+        }
+
+        return index;
     }
 };
 
