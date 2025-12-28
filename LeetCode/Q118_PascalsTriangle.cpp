ad09f8a5ee664e581e6272f19bb306cee2d0edd0
PrinceG-2k6 <princegupta14311@gmail.com>
2025-12-28 21:28:23 +0530

    .

---
+++ LeetCode/Q118_PascalsTriangle.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        
+    }
+};
+
+
+int main()
+{
+    Solution sol;
+    
+    vector<vector<int>> ans = sol.generate(5);
+    for(vector<int> a : ans){
+        for(int n : a){
+            cout<< n << " ";
+        }
+        cout<<endl;
+    }
+    
+}
