ca955c25a0a4387587f0bbfc3e3d7b1a8dd540b4
PrinceG-2k6 <princegupta14311@gmail.com>
2025-12-29 15:52:44 +0530

    .

--- LeetCode/Q1_BalancedNo.cpp
+++ LeetCode/Q1_BalancedNo.cpp
@@ -2,37 +2,38 @@
 using namespace std;
 class Solution {
 public:
-    long long countBalanced(long long low, long long high) {
-        long long difference = high - low;
-        long long count =0;
-        for(long long i = low;i<=high;i++){
-            long long x =i;
-            long long sum = 0;
-            int choice = 1;
-            while(x){
-                int a = x%10;
-                if(choice){
-                    sum+=a;
-                    choice = 0;
-                }
-                else{
-                    sum-=a;
-                    choice = 1;
-                }
-                x/=10;
-            }
-            sum = abs(sum);
-            if(i%11==0 && (sum==0||sum%11!=0) ){
-                count++;
-            }
-        }
+    long long dp[20][205][2][2];
+    string s;
 
-        return count;
+    long long f(int i, int d, int t, int st) {
+        if (i == s.size()) return st && d == 100;
+        if (dp[i][d][t][st] != -1) return dp[i][d][t][st];
+        long long r = 0;
+        int lim = t ? s[i] - '0' : 9;
+        for (int x = 0; x <= lim; x++) {
+            int nd = d;
+            int ns = st || x;
+            if (ns) nd += (i % 2 == 0 ? x : -x);
+            r += f(i + 1, nd, t && x == lim, ns);
+        }
+        return dp[i][d][t][st] = r;
+    }
+
+    long long numberOfBalancedIntegers(long long x) {
+        if (x <= 0) return 0;
+        s = to_string(x);
+        memset(dp, -1, sizeof(dp));
+        return f(0, 100, 1, 0);
+    }
+
+    int countBalanced(long long low, long long high) {
+        return numberOfBalancedIntegers(high) - numberOfBalancedIntegers(low - 1);
     }
 };
+
 int main()
 {
     Solution sol;
-    cout<<sol.countBalanced(238,329)<<endl;
+    cout<<sol.countBalanced(218,329)<<endl;
     
 }   
