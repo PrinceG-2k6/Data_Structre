ddd0e00bf67c8dc7970e43c884966995e17b7657
PrinceG-2k6 <princegupta14311@gmail.com>
2025-12-29 14:50:59 +0530

    .

---
+++ LeetCode/Q756_Pyramid.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+private:
+unordered_map<string, vector<char>> allowedTops_;
+unordered_map<string, bool> memo_;
+    bool generateNextLevels(string& previous, string current) { 
+        if (previous.size() == 1) { 
+            return true;
+        }
+        if (previous.length() - 1 == current.length()) {
+            if (memo_.find(current) != memo_.end()) { 
+                return memo_[current];
+            }
+            bool result = generateNextLevels(current, "");
+            memo_[current] = result;
+            return result;
+        }
+        int idx = current.length();
+        for (const auto& top : allowedTops_[previous.substr(idx, 2)]) {
+            if (generateNextLevels(previous, current+top)) { 
+                return true;
+            }
+        }
+        return false;
+    }
+public:
+    bool pyramidTransition(string bottom, vector<string>& allowed) {
+        for (const auto& triple : allowed) { 
+            allowedTops_[triple.substr(0,2)].push_back(triple.back());
+        }
+        return generateNextLevels(bottom, "");
+    }
+};
+int main()
+{
+    Solution sol;
+    vector<string> allowed ={"BCC","CDE","CEA","FFF"};
+    cout<<sol.pyramidTransition("BCD",allowed)<<endl;
+    
+}   
