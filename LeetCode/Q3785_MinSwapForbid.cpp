6a2b7de922af2252b31a501eb5804feb0130ce3f
PrinceG-2k6 <princegupta14311@gmail.com>
2025-12-30 17:45:47 +0530

    .

--- LeetCode/Q3785_MinSwapForbid.cpp
+++ LeetCode/Q3785_MinSwapForbid.cpp
@@ -2,28 +2,43 @@
 using namespace std;
 
 class Solution {
-    int count = 0;
 public:
     int minSwaps(vector<int>& nums, vector<int>& forbidden) {
-        int n = nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]==forbidden[i]){
-                int j=i+1;
-                while(j%n!=i){
-                    if(nums[i]!=forbidden[j] && nums[j]!=forbidden[i]){
-                        
-                        int t = nums[i];
-                        nums[i]=nums[j];
-                        nums[j]=t;
-                        count++;
-                        break;
-                    }
-                    j++;
+        int size = nums.size();
+        int cand_n = 0, n_cnt = 0;
+        int not_n = 0;
+        int all_forb = 0;
+
+        // First pass: count forbidden positions and find majority candidate
+        for(int ii=0; ii<size; ++ii) {
+            int n = nums[ii];
+            if(n == forbidden[ii]) {
+                all_forb++;
+                if(n == cand_n) n_cnt++;
+                else if(n_cnt) n_cnt--;
+                else {
+                    cand_n = n;
+                    n_cnt = 1;
                 }
             }
         }
 
-        return count;
+        n_cnt = 0;
+
+        // Second pass: verify candidate and count valid positions
+        for(int ii=0; ii<size; ++ii) {
+            int n = nums[ii];
+            // How many forbidden positions actually contain cand_n
+            if(n == cand_n && n == forbidden[ii]) n_cnt++;  
+            // How many valid positions can accept cand_n
+            else if(n != cand_n && cand_n != forbidden[ii]) not_n++;  
+        }
+
+        // Impossible if not enough valid places
+        if(n_cnt > not_n) return -1;
+
+        // Minimum swaps needed
+        return max(n_cnt, (all_forb+1)/2);
     }
 };
 int main()
