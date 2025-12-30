21368ce0c4a9641c8b13fbf0a1ee3d297fb13002
PrinceG-2k6 <princegupta14311@gmail.com>
2025-12-30 15:56:30 +0530

    .

--- LeetCode/Q3783_MirrorDistance.cpp
+++ LeetCode/Q3783_MirrorDistance.cpp
@@ -4,16 +4,16 @@ using namespace std;
 class Solution {
 public:
     int mirrorDistance(int n) {
-        long long x = n;
-        long long reverse = 0;
+        int x = n;
+        int reverse = 0;
         while(x){
             int t = x%10;
             reverse =(reverse*10)+t;
             x/=10;
         }
         
-        long long ans = abs(n-reverse);
-        return ans;
+        return abs(n-reverse);
+        
     }
 };
 int main()
