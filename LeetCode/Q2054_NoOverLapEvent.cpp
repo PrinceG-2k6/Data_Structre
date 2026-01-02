895241239aa5dbb21d40f2aff04028af47b74652
PrinceG-2k6 <princegupta14311@gmail.com>
2026-01-02 13:03:14 +0530

    .

---
+++ LeetCode/Q2054_NoOverLapEvent.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxTwoEvents(vector<vector<int>>& events) {
+        
+    }
+};
+int main()
+{
+    Solution sol;
+    vector<vector<int>> events = {{1,3,2},{4,5,2},{2,4,3}};
+    cout<<sol.maxTwoEvents(events);
+    
+}
