fc15f0a2776a2f19b3ed4d7d7664833736baaf48
PrinceG-2k6 <princegupta14311@gmail.com>
2026-01-03 10:31:45 +0530

    .

--- Concept/Recursion/L2_power.cpp
+++ Concept/Recursion/L2_power.cpp
@@ -1,13 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int power(int x,int n){
-    if(n==0){
+int power(int x, int n)
+{
+    if (n == 0)
+    {
         return 1;
     }
-    return x*power(x,n-1);
+    return x * power(x, n - 1);
 }
 
-int main(){
-    cout<<power(2,0);
+int powerLog(int *count ,int x, int n)
+{
+    if (n == 0)
+        return 1;
+    *count +=1;
+    int half = powerLog(count,x, n / 2);
+
+    if (n % 2 == 0)
+        return half * half;
+    else
+        return x * half * half;
+}
+
+
+int main()
+{
+    cout << power(2, 0) << endl;
+    int count =0;
+    cout << powerLog(&count,2, 8) << endl;
+    cout<<count<<endl;
 }
--- Concept/Recursion/tempCodeRunnerFile.cpp
+++ Concept/Recursion/tempCodeRunnerFile.cpp
@@ -1,13 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int power(int x,int n){
-    if(n==0){
+int power(int x, int n)
+{
+    if (n == 0)
+    {
         return 1;
     }
-    return x*power(x,n-1);
+    return x * power(x, n - 1);
 }
 
-int main(){
-    cout<<power(2,0);
+int powerLog(int x, int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    if (n % 2 == 0)
+    {
+        return x*powerLog(x, n / 2) * powerLog(x, n / 2);
+    }
+    else
+    {
+        return x*powerLog(x, n / 2) * powerLog(x, n / 2) * powerLog(x, 1);
+    }
+}
+
+int main()
+{
+    cout << power(2, 0) << endl;
+    ;
+    cout << powerLog(2, 8) << endl;
 }
