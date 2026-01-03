76765e879b633b9045f8bb27e1feb85af5089342
PrinceG-2k6 <princegupta14311@gmail.com>
2026-01-03 11:25:06 +0530

    .

---
+++ Concept/Recursion/L5_Array.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void traverse(int a[],int n,int size){
+    if(n>=size)return;
+    cout<<a[n]<<" ";
+    traverse(a,n+1,size);
+}
+void traverseRev(int a[],int n,int size){
+    if(n>=size)return;
+    traverseRev(a,n+1,size);
+    cout<<a[n]<<" ";
+}
+
+
+int MaxArray(int a[],int n,int size){
+    if(n>=size)return INT_MIN;
+    return max(a[n],MaxArray(a,n+1,size));
+}
+
+
+int main(){
+    int a[]={1,2,3,4,5,6};
+    int size = 6;
+    int b[]={2,5,2,4,8,4};
+    traverse(a,0,size);
+    printf("\n");
+    traverseRev(a,0,size);
+    printf("\n");
+    // i.e. (array,start_idx,end_idx)
+    cout<<MaxArray(b,0,6)<<endl;
+
+}
