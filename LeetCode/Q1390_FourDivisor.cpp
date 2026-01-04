409f0f004e1a65c6f4cf315e163ea1f45ef3b73e
PrinceG-2k6 <princegupta14311@gmail.com>
2026-01-04 12:40:06 +0530

    .

--- LeetCode/Q1390_FourDivisor.cpp
+++ LeetCode/Q1390_FourDivisor.cpp
@@ -1,11 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution
-{
+class Solution {
 public:
-    int sumFourDivisors(vector<int> &nums)
-    {
-        map<int, int> primeCube = {
-            {8, 2}, {27, 3}, {125, 5}, {343, 7}, {1331, 11}, {2197, 13}, {4913, 17}, {6859, 19}, {12167, 23}, {24389, 29}, {29791, 31}, {50653, 37}, {68921, 41}, {79507, 43}};
+    int sumFourDivisors(vector<int>& nums) {
+        int res = 0;
+        for (int n : nums) {
+            int val = sumOne(n);
+            if (val != -1) res += val;
+        }
+        return res;
+    }
+
+    int sumOne(int n) {
+        int p = round(cbrt(n));
+        if ((long long)p * p * p == n && isPrime(p)) {
+            return 1 + p + p*p + p*p*p;
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                int a = i, b = n / i;
+                if (a != b && isPrime(a) && isPrime(b)) {
+                    return 1 + a + b + n;
+                }
+                return -1;
+            }
+        }
+        return -1;
+    }
+
+    bool isPrime(int x) {
+        if (x < 2) return false;
+        for (int i = 2; i * i <= x; i++) {
+            if (x % i == 0) return false;
+        }
+        return true;
     }
 };
+
+int main(){
+    Solution sol;
+    vector<int> nums = {21,4,7,21,8};
+    cout<<sol.sumFourDivisors(nums);
+}
