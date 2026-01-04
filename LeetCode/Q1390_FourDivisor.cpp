a0a0945c6c860b9a7d17ca5282d7f441e1b981b2
PrinceG-2k6 <princegupta14311@gmail.com>
2026-01-04 12:33:45 +0530

    .

---
+++ LeetCode/Q1390_FourDivisor.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int sumFourDivisors(vector<int> &nums)
+    {
+        map<int, int> primeCube = {
+            {8, 2}, {27, 3}, {125, 5}, {343, 7}, {1331, 11}, {2197, 13}, {4913, 17}, {6859, 19}, {12167, 23}, {24389, 29}, {29791, 31}, {50653, 37}, {68921, 41}, {79507, 43}};
+    }
+};
