b05fc25ce25928be565009c1c2e433a79c6520c4
PrinceG-2k6 <princegupta14311@gmail.com>
2026-01-07 10:48:59 +0530

    .

--- LeetCode/Q1339_MaxProductBT.cpp
+++ LeetCode/Q1339_MaxProductBT.cpp
@@ -1,15 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Definition for singly-linked list.
-struct TreeNode
-{
+
+// Definition for a binary tree node.
+struct TreeNode {
     int val;
-    TreeNode *next;
-    TreeNode() : val(0), next(nullptr) {}
-    TreeNode(int x) : val(x), next(nullptr) {}
-    TreeNode(int x, TreeNode *next) : val(x), next(next) {}
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+
 
 class Solution {
 public:
