af646c43c31b6a65fde40212cee511b0c87ed6eb
PrinceG-2k6 <princegupta14311@gmail.com>
2026-01-10 13:04:44 +0530

    .

--- LeetCode/Q712_MinASCIIDel.cpp
+++ LeetCode/Q712_MinASCIIDel.cpp
@@ -4,49 +4,34 @@ using namespace std;
 
 class Solution {
 public:
-    int dp[1001][1001];
-    
-    Solution() {
-        memset(dp, -1, sizeof(dp));
-    }
-    
-    int func(string s1, string s2, int i, int j){
-        int n = s1.size(),m =s2.size();
-        if(i==n){
-            int s=0;
-            while(j<m){
-                s+=s2[j];j++;
-            }
-            return s;
-        }
-        if(j==m){
-            int s=0;
-            while(i<n){
-                s+=s1[i];i++;
-            }
-            return s;
-        }
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        if(s1[i]==s2[j]){
-            dp[i][j] = func(s1,s2,i+1,j+1);
-            return dp[i][j];
-        }
-
-        int skip_s1 = s1[i] + func(s1,s2,i+1,j);
-        int skip_s2 = s2[j] + func(s1,s2,i,j+1);
-
-        dp[i][j]=min(skip_s1,skip_s2);
-        return dp[i][j];
-    }
-
     int minimumDeleteSum(string s1, string s2) {
-        return func(s1,s2,0,0);
+        int m = s1.length();
+        int n = s2.length();
+        vector<int> dp(n + 1, 0);
 
+        for(int j = 1; j <= n; j++) {
+            dp[j] = dp[j - 1] + s2[j - 1];
+        }
+
+        for(int i = 1; i <= m; i++) {
+            int prev = dp[0];
+            dp[0] += s1[i - 1];
+
+            for(int j = 1; j <= n; j++) {
+                int temp = dp[j];
+
+                if(s1[i - 1] == s2[j - 1]) {
+                    dp[j] = prev;
+                } else {
+                    dp[j] = min(dp[j] + s1[i - 1], dp[j - 1] + s2[j - 1]);
+                }
+                prev = temp;
+            }
+        }
+        return dp[n];
+        
     }
 };
-
 int main(){
     Solution sol;
     cout<<sol.minimumDeleteSum("delete","leet")<<endl;
