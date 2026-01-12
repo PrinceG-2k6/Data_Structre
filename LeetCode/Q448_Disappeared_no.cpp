df320140d22e773895930d2f864ffb22e85b413a
PrinceG-2k6 <princegupta14311@gmail.com>
2026-01-12 22:15:24 +0530

    .

---
+++ LeetCode/Q448_Disappeared_no.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int Distance =0;
+
+        for(int i=1;i<points.size();i++){
+            int a  = abs(points[i][0]-points[i-1][0]);
+            int b  = abs(points[i][1]-points[i-1][1]);
+
+            Distance += max(a,b);
+
+        }
+
+        return Distance;
+    }
+};
+
+int main (){
+    Solution sol;
+    vector<vector<int>> points = {{1,1},{3,4},{-1,0}};
+    cout<<sol.minTimeToVisitAllPoints(points);
+}
