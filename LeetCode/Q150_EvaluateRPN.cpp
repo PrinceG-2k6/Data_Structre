f4db220d8d0e78acdce5d55077022ab9784cc345
PrinceG-2k6 <princegupta14311@gmail.com>
2026-01-13 21:40:34 +0530

    .

--- LeetCode/Q150_EvaluateRPN.cpp
+++ LeetCode/Q150_EvaluateRPN.cpp
@@ -5,9 +5,10 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         vector<int> stack;
-        for(char c: tokens){
-            if(c == '+'){
-                int a = stack
+        for(string c: tokens){
+            if(c[0] == '+'){
+                char a = stack.pop_back();
+                char b = stack.pop_back();
             }
         }
     }
