9c272bdd6762e094546041ca405eefc4c272cd1c
PrinceG-2k6 <princegupta14311@gmail.com>
2026-01-13 21:51:40 +0530

    .

--- LeetCode/Q636_STackTimelog.cpp
+++ LeetCode/Q636_STackTimelog.cpp
@@ -7,3 +7,12 @@ public:
         
     }
 };
+
+int main(){
+    Solution sol;
+    vector<string> logs = {"0:start:0","1:start:2","1:end:5","0:end:6"};
+    vector<int> ans = sol.exclusiveTime(2,logs);
+    for(int n : ans ){
+        cout<< n << " ";
+    }
+}
