4c5e6d9dcad10c7e574bef925625cc9284c37b3e
PrinceG-2k6 <princegupta14311@gmail.com>
2026-01-15 21:31:15 +0530

    .

--- LeetCode/Q1_ExclusiveTime.cpp
+++ LeetCode/Q1_ExclusiveTime.cpp
@@ -3,44 +3,34 @@ using namespace std;
 class Solution {
 public:
     vector<int> exclusiveTime(int n, vector<string>& logs) {
-        vector<int> ans(n,0);
-        string s = logs[0];
-            string part;
-            int start, end;
+        vector<int> freq(n,0);
 
-            stringstream ss(s);
-
-            getline(ss, part, ':'); // "0"
-            start = stoi(part);
-
-            getline(ss, part, ':'); // "end" (ignored)
-
-            getline(ss, part, ':'); // "5"
-            end = stoi(part);
-
-            ans[start] += end;
-        for (int i = 1; i < logs.size(); i++) {
-            s = logs[i];
-            int nextStart,nextEnd;
-
-            stringstream ss(s);
-
-            getline(ss, part, ':'); // "0"
-            nextStart= stoi(part);
-
-            getline(ss, part, ':'); // "end" (ignored)
-
-            getline(ss, part, ':'); // "5"
-            nextEnd = stoi(part);
-
-            ans[start] += nextEnd - end;
-            end = nextEnd;
+        stack<tuple<string,int,int>> fxn;
+        for(string& log:logs){
+            string id,status,timestamp;
+            int runtime,fee_init=0;
+            stringstream ss(log);
+            getline(ss,id,':');
+            getline(ss,status,':');
+            getline(ss,timestamp,':');
+            if(status == "start"){
+                fxn.push({id,stoi(timestamp),fee_init});
+            }
+            else{
+                int starttime=get<1>(fxn.top());
+                int fee = get<2>(fxn.top());
+                int time = stoi(timestamp)-starttime+1;
+                runtime = time - fee;
+                
+                freq[stoi(id)]+=runtime;
+                fxn.pop();
+                if(!fxn.empty())
+                    get<2>(fxn.top())+=time;
+            }
         }
-
-        return ans;
-    } 
+        return freq;
+    }
 };
-
 int main(){
     Solution sol;
     vector<string> logs = {"0:start:0","1:start:2","1:end:5","0:end:6"};
