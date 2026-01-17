0465980b1f3fffc11896a72b20bb1b8507d5b8b4
PrinceG-2k6 <princegupta14311@gmail.com>
2026-01-17 19:51:18 +0530

    .

---
+++ Concept/Stack/Q496_NextGreateer.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> next;
+        stack<int> s;
+
+        for (int i = nums2.size() - 1; i >= 0; --i) {
+            while (!s.empty() && s.top() <= nums2[i]) {
+                s.pop();
+            }
+            next[nums2[i]] = s.empty() ? -1 : s.top();
+            s.push(nums2[i]);
+        }
+
+        vector<int> res;
+        for (int num : nums1) {
+            res.push_back(next[num]);
+        }
+        return res;
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<int> num1 = {4,1,2};
+    vector<int> num2 = {1,3,4,2};
+
+    vector<int> ans = sol.nextGreaterElement(num1,num2);
+}
