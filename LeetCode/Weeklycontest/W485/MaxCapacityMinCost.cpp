3df495c7cb13b4d999a1a671c8d26440ef95302d
PrinceG-2k6 <princegupta14311@gmail.com>
2026-01-18 09:03:09 +0530

    .

--- LeetCode/Weeklycontest/W485/MaxCapacityMinCost.cpp
+++ LeetCode/Weeklycontest/W485/MaxCapacityMinCost.cpp
@@ -1,47 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int maxCapacity(vector<int> &costs, vector<int> &capacity, int budget)
-    {
+    int maxCapacity(vector<int>& costs, vector<int>& capacity, int budget) {
         int n = costs.size();
 
-        vector<pair<int, int>> machines;
-        for (int i = 0; i < n; i++)
-        {
+        auto temp = make_pair(costs, capacity);
+
+        vector<pair<int,int>> machines;
+        for (int i = 0; i < n; i++) {
             machines.push_back({costs[i], capacity[i]});
         }
 
-        sort(machines.begin(), machines.end());
+        sort(machines.begin(), machines.end()); 
 
-        int maxCapacity = 0;
+        vector<int> bestCap(n);
+        bestCap[0] = machines[0].second;
+        for (int i = 1; i < n; i++) {
+            bestCap[i] = max(bestCap[i-1], machines[i].second);
+        }
 
-        for (int i = 0; i < n; i++)
-        {
-            if (machines[i].first < budget)
-            {
-                maxCapacity = max(maxCapacity, machines[i].second);
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (machines[i].first < budget) {
+                ans = max(ans, machines[i].second);
             }
         }
 
-        int left = 0, right =n-1;
-        while (left < right) {
-            int totalCost = machines[left].first + machines[right].first;
-        if (totalCost < budget) {
-            for (int i = right; i > left; i--) {
-                maxCapacity = max(maxCapacity,machines[left].second+machines[i].second);
-            }
-            left++;
-        } else {
-            right--;
-        }
-    }
+        for (int i = 1; i < n; i++) {
+            int remaining = budget - machines[i].first - 1;
+            if (remaining < 0) continue;
 
-        return maxCapacity;
+            int l = 0, r = i - 1, pos = -1;
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (machines[mid].first <= remaining) {
+                    pos = mid;
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+
+            if (pos != -1) {
+                ans = max(ans, machines[i].second + bestCap[pos]);
+            }
+        }
+
+        return ans;
     }
 };
+
 
 int main()
 {
