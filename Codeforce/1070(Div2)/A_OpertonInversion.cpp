adb4278440eeba5ceb4ac8926e0688303bcfe58e
PrinceG-2k6 <princegupta14311@gmail.com>
2026-01-18 11:39:38 +0530

    .

--- Codeforce/1070(Div2)/A_OpertonInversion.cpp
+++ Codeforce/1070(Div2)/A_OpertonInversion.cpp
@@ -1,4 +1,5 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
 using namespace std;
 
 int main(){
@@ -8,6 +9,18 @@ int main(){
     while(t--){
         int n;
         cin>>n;
-        for(int i=0;i<n;i++); 
+        vector<int> arr(n);
+        for(int i=0;i<n;i++)cin>>arr[i];
+        int j = 0;
+        int ans =0;
+        for(int i=1;i<n;i++){
+            if(arr[i]>=arr[j]){
+                j=i;
+            }else{
+                ans++;
+            }
+        }
+
+        cout<<ans<<endl;        
     }
 }
