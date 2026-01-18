2feaeec2480212a9d8850013f42d0c4174485099
PrinceG-2k6 <princegupta14311@gmail.com>
2026-01-18 08:52:53 +0530

    .

---
+++ LeetCode/Weeklycontest/W485/tempCodeRunnerFile.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxCapacity(vector<int> &costs, vector<int> &capacity, int budget)
+    {
+        int n = costs.size();
+
+        vector<pair<int, int>> machines;
+        for (int i = 0; i < n; i++)
+        {
+            machines.push_back({costs[i], capacity[i]});
+        }
+
+        sort(machines.begin(), machines.end());
+
+        int maxCapacity = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (machines[i].first < budget)
+            {
+                maxCapacity = max(maxCapacity, machines[i].second);
+            }
+        }
+
+        int left = 0, right = n - 1;
+        while (left < right)
+        {
+            int totalCost = machines[left].first + machines[right].first;
+
+            if (totalCost < budget)
+            {
+                maxCapacity = max(maxCapacity,
+                                  machines[left].second + machines[right].second);
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+        left = 0, right = n - 1;
+        while (left < right)
+        {
+            int totalCost = machines[left].first + machines[right].first;
+
+            if (totalCost < budget)
+            {
+                maxCapacity = max(maxCapacity,
+                                  machines[left].second + machines[right].second);
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return maxCapacity;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> cost = {1, 7, 3};
+    vector<int> capacity = {7, 3, 5};
+    cout << sol.maxCapacity(cost, capacity, 13);
+}
