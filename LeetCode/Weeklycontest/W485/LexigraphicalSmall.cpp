ca5bd9348f30da822c4c46886fd52ce9dc219fa2
PrinceG-2k6 <princegupta14311@gmail.com>
2026-01-18 09:34:38 +0530

    .

--- LeetCode/Weeklycontest/W485/LexigraphicalSmall.cpp
+++ LeetCode/Weeklycontest/W485/LexigraphicalSmall.cpp
@@ -3,28 +3,32 @@ using namespace std;
 class Solution {
 public:
     string lexSmallestAfterDeletion(string s) {
+
+        int n = s.size();
         vector<int> freq(26, 0);
-        for (char c : s) {
-            freq[c - 'a']++;
+        for (char c : s) freq[c - 'a']++;
+
+        vector<char> suffixMin(n);
+        suffixMin[n - 1] = s[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = min(s[i], suffixMin[i + 1]);
         }
 
-        string st;
-        
-        reverse(s.begin(), s.end());
+        string result;
 
-        for (char c : s) {
-            freq[c - 'a']--;
+        for (int i = 0; i < n; i++) {
+            char c = s[i];
 
-            while (!st.empty() &&
-                   st.back() > c &&
-                   freq[st.back() - 'a'] > 0) {
-                st.pop_back();
+            if (freq[c - 'a'] > 1 && i + 1 < n && c > suffixMin[i + 1]) {
+                freq[c - 'a']--;
+                continue;
             }
 
-            st.push_back(c);
+            result.push_back(c);
+            freq[c - 'a']--;
         }
 
-        return st;
+        return result;
     }
 };
 
